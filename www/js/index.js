ca6505119d3a38cbe11062ab282cc85b4f762438
michalhavel <michal.havel@cadstudio.cz>
2018-03-06 13:54:19 +0100

    Doplneni HASH modelu - roboticka svarovna

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,7 @@ var options = {
     getAccessToken: getForgeToken
 }
 
-var documentId = 'urn:YOUR-URN';
+var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTAyLTI4LTA4LTI1LTE0LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL0QtNjIwLTAwMC1ObyUyMEV4cHJlc3MuZHdmeA';
 
 Autodesk.Viewing.Initializer(options, function onInitialized() {
     Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
